1213b28926f551bc6eb861ab0b6f8749c7171d21
Adedigba Oluwadarasimi <boatech4good@gmail.com>
2023-05-15 22:56:07 +0100

    ..

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,12 +1,15 @@
 import Meta from '@/components/Meta';
 import { Container } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function Welcome() {
   return (
     <>
       <Meta title="Welcome" />
       <Container>
-        <div className=''></div>
+        <div className='flex'>
+          
+        </div>
       </Container>
     </>
   );
