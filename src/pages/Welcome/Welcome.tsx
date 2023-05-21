784c1ed6b61cddd8957369070ea5dcbf395ecb0f
Adedigba Oluwadarasimi <boatech4good@gmail.com>
2023-05-21 23:47:19 +0100

    yuppy, it's working out

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,4 +1,6 @@
 import Meta from '@/components/Meta';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import {styled} from '@mui/material/styles'
 import { 
   Container,
@@ -10,8 +12,6 @@ import {
   InputBase,
   TextField
   } from '@mui/material';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import SearchIcon from '@mui/icons-material/Search';
 import 'typeface-manrope'
 import { borderColor } from '@mui/system';
 
@@ -19,18 +19,14 @@ import { borderColor } from '@mui/system';
 const SearchBox = styled(InputBase)(({ theme }) => ({
   '& .MuiInputBase-input': {
     borderRadius: 10,
-    border: '1px solid #d9dcdd',
-    backgroundColor: '#f8f9f8',
+    backgroundColor: '#f7f8f8',
     height: '38px',
     padding: '5px 35px 5px 15px',
     fontSize: '14px',
-    color: '#97a0ae',
+    color: '#949dab',
     fontFamily: 'Manrope',
     tranition: 'all 5s ease'
   },
-  '& .MuiInputBase-input:focus-visible, .MuiInputBase-input:hover': {
-    borderColor: theme.palette.primary.main
-  }
 }))
 
 const Buttons = styled(Button)({
@@ -44,11 +40,11 @@ function Welcome() {
     <>
       <Meta title="Home" />
       <Box display={'flex'} flexDirection={'column'} gap={3} alignItems={'center'} py={3} px={3}>
-        <Stack direction={'row'} spacing={1} width={'100%'}>
-          <IconButton size='large'>
-            <ChevronLeftIcon sx={{ color: 'black'}}/>
+        <Stack direction={'row'} alignItems={'center'} spacing={1} width={'100%'}>
+          <IconButton size='medium' disableRipple sx={{width: '32px', p: 0}}>
+            <FontAwesomeIcon size="1x" color='black' icon={faChevronLeft} />
           </IconButton>
-          <SearchBox endAdornment={<IconButton size='large' sx={{position: 'absolute', right: 5,}}><SearchIcon  /></IconButton>} fullWidth placeholder='Search for a product or cat...' sx={{fontWeight: 500}} />
+          <SearchBox endAdornment={<IconButton size='medium' disableRipple sx={{position: 'absolute', right: 8,}}><FontAwesomeIcon icon={faMagnifyingGlass} color='#949dab'/></IconButton>} fullWidth placeholder='Search for a product or cat...' sx={{fontWeight: 400}} />
         </Stack>
           <Stack direction={'row'} gap={4} height={'40px'} width={'100%'}>
             <Buttons variant='outlined' sx={{height: '100%', minWidth: '40px',}}></Buttons>
